c4e06059494c721b0cfa8d401338920da2e3a800
Nikita <nks.off@gmail.com>
2017-06-30 00:09:21 +0300

    <init> add simple /echo command

--- set.php
+++ set.php
@@ -1,17 +1,15 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 $bot_api_key = getenv('TELEGRAM_BOT_KEY');
 $bot_username = getenv('TELEGRAM_BOT_NAME');
 
+$hook_url = 'https://zsong.ru/telegram-bot';
 try {
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-    $telegram->addCommandsPaths([
-        __DIR__ . '/Commands',
-    ]);
-    $telegram->enableLimiter();
-    $telegram->handle();
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    Longman\TelegramBot\TelegramLog::error($e);
+    echo $e->getMessage();
 }
